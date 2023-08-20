0235eff54e84d39ab920b8536b8cc012b7f607c1
Kyu1523 <k486498@gmail.com>
2023-08-20 16:06:05 -0400

    Finished

---
+++ script.js
@@ -0,0 +1,69 @@
+let playerWins = 0;
+let ComputerWins = 0;
+for(let i = 0; i < 5; i++){
+    let userChoice = prompt("Type either, rock, paper, or scissors");
+    game(userChoice);
+    console.log("Player has won: " + playerWins + " times. Computer has won: " + ComputerWins);
+}
+
+function getComputerChoice(){
+    let choice = Math.floor(Math.random()*3);
+    let CpuChoice;
+    switch(choice){
+        case 0:
+            return CpuChoice = "rock";
+        case 1:
+            return CpuChoice = "scissors";
+        case 2:
+            return CpuChoice = "paper";
+    }
+    return "";
+}
+
+function playRound(user,Cpu){
+    if(user == "rock"){
+        if(Cpu == "rock"){
+            return "Tie";
+        }
+        else if(Cpu == "paper"){
+            return "Cpu Wins";
+        }
+        else{
+            return "Player Wins";
+        }
+    }
+    else if(user == "paper"){
+        if(Cpu == "rock"){
+            return "Player Wins";
+        }
+        else if(Cpu == "paper"){
+            return "Tie";
+        }
+        else{
+            return "Cpu Wins";
+        }
+    }
+    else if(user == "scissors"){
+        if(Cpu == "rock"){
+            return "Cpu Wins";
+        }
+        else if(Cpu == "paper"){
+            return "Player Wins";
+        }
+        else{
+            return "Tie";
+        }
+    }
+}
+
+function game(userChoice){
+let CpuChoice = getComputerChoice();
+console.log("Computer chooses: " + CpuChoice);
+console.log(playRound(userChoice.toLowerCase(),CpuChoice));
+    if(playRound(userChoice.toLowerCase(),CpuChoice) == "Cpu Wins"){
+        ComputerWins++;
+    }
+    else if(playRound(userChoice.toLowerCase(),CpuChoice) == "Player Wins"){
+        playerWins++;
+    }
+}
